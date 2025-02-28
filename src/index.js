56ac02d8e22f35ca00838e1b1e27555d8d8fc704
Dmtro_Tr <Di4gamegem@gmail.com>
2025-02-28 20:15:50 +0200

    Step 3 and 4

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-const message = "Hello world";
+import { initMongoConnection } from './db/initMongoConnection.js';
+import { setupServer } from './server.js';
 
-console.log(message);
+setupServer();
+initMongoConnection();
--- src/server.js
+++ src/server.js
@@ -2,33 +2,53 @@ import express from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
 import dotenv from 'dotenv';
+import { initMongoConnection } from './db/initMongoConnection.js';
+// import { getEnvVar } from './utils/getEnvVar.js';
 
 dotenv.config();
 
 const PORT = Number(process.env.PORT) || 3000;
 
-export function setupServer() {
-  const app = express();
+export async function setupServer() {
+  try {
+    await initMongoConnection();
 
-  app.use(cors());
+    const app = express();
 
-  app.use(
-    pino({
-      transport: {
-        target: 'pino-pretty',
-      },
-    }),
-  );
+    app.use(cors());
 
-  app.use('*', (req, res, next) => {
-    res.status(404).json({
-      message: 'Not found',
+    app.use(
+      pino({
+        transport: {
+          target: 'pino-pretty',
+        },
+      }),
+    );
+
+    app.get('/contacts', async (req, res) => {
+      const contacts = await contacts.find();
+
+      res.json(contacts);
     });
-  });
 
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-  });
+    app.get('/contacts/:contactId', (req, res) => {
+      const { contactId } = req.params;
+
+      res.send(`Contacts ${contactId}`);
+    });
+
+    app.use('*', (req, res, next) => {
+      res.status(404).json({
+        message: 'Not found',
+      });
+    });
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 setupServer();
