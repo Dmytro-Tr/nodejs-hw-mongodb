df578ea79fefa72a8a243d58f755fa6d4df6a855
Dmtro_Tr <Di4gamegem@gmail.com>
2025-03-02 11:46:29 +0200

    fixed bugs

--- src/server.js
+++ src/server.js
@@ -2,16 +2,12 @@ import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
-import { initMongoConnection } from './db/initMongoConnection.js';
 import { getAllContacts, getContactById } from './services/contacts.js';
-import { getEnvVar } from './utils/getEnvVar.js';
 
-const PORT = Number(getEnvVar('PORT', '3000')) || 3000;
+const PORT = Number(process.env.PORT) || 3000;
 
 export async function setupServer() {
   try {
-    await initMongoConnection();
-
     const app = express();
     app.use(express.json());
     app.use(cors());
@@ -28,6 +24,7 @@ export async function setupServer() {
       const contacts = await getAllContacts();
 
       res.status(200).json({
+        status: 200,
         message: 'Successfully found contacts!',
         data: contacts,
       });
@@ -39,12 +36,14 @@ export async function setupServer() {
 
       if (!contact) {
         res.status(404).json({
+          status: 404,
           message: 'Contact not found',
         });
         return;
       }
 
       res.status(200).json({
+        status: 200,
         message: 'Successfully found contact with id {contactId}!',
         data: contact,
       });
@@ -52,12 +51,14 @@ export async function setupServer() {
 
     app.use('*', (req, res, next) => {
       res.status(404).json({
+        status: 404,
         message: 'Not found',
       });
     });
 
     app.use((err, req, res, next) => {
       res.status(500).json({
+        status: 500,
         message: 'Something went wrong',
       });
     });
