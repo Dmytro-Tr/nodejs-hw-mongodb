010e5ecffff42ec6f3e1307e3acc89727148298d
Dmtro_Tr <Di4gamegem@gmail.com>
2025-04-06 19:32:18 +0300

    almost working

--- src/server.js
+++ src/server.js
@@ -7,14 +7,7 @@ import router from './routers/index.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { UPLOAD_DIR } from './constants/index.js';
-// import { swaggerDocs } from './middlewares/swaggerDocs.js';
-import swaggerUIExpress from 'swagger-ui-express';
-import * as fs from 'node:fs';
-import path from 'node:path';
-
-const swaggerDocument = JSON.parse(
-  fs.readFileSync(path.resolve('docs', 'swagger.json'), 'utf-8'),
-);
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 const PORT = Number(process.env.PORT) || 3000;
 
@@ -39,11 +32,7 @@ export async function setupServer() {
 
     app.use('/uploads', express.static(UPLOAD_DIR));
 
-    app.use(
-      '/api-docs',
-      swaggerUIExpress.serve,
-      swaggerUIExpress.setup(swaggerDocument),
-    );
+    app.use('/api-docs', swaggerDocs());
   } catch (error) {
     console.error(error);
   }
